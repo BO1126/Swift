8d8385159c92541d59357b1236138dc6c60c5d29
jungwoo Lee <b5nnnal88@gmail.com>
2021-03-29 22:37:53 +0900

    swift-0329

---
+++ swift-practice/Shared/swift_practiceApp.swift
@@ -0,0 +1,17 @@
+//
+//  swift_practiceApp.swift
+//  Shared
+//
+//  Created by 이정우 on 2021/03/29.
+//
+
+import SwiftUI
+
+@main
+struct swift_practiceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
