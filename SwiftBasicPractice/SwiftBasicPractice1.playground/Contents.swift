57e6c06f1f1d3a9ca13cff7c9e68ccedabdf2f06
jungwoo Lee <b5nnnal88@gmail.com>
2021-04-03 12:57:58 +0900

    swift-0403
    swift data type

---
+++ SwiftBasicPractice/SwiftBasicPractice1.playground/Contents.swift
@@ -0,0 +1,17 @@
+import UIKit
+
+//상수와 변수 선언
+// 상수 선언
+let constant: String = "변경이 불가능한 값 let"
+var variable: String = "변경이 가능한 값 var"
+
+// 상수 선언 후에 나중에 값 할당하기
+let sum: Int
+let inputA = 100
+let inputB = 200
+
+sum = inputA + inputB
+
+print(sum)
+
+
---
+++ SwiftBasicPractice/SwiftBasicPractice2.playground/Contents.swift
@@ -0,0 +1,33 @@
+import UIKit
+//swift의 기본 데이터 타입
+// Bool, Int, UInt, Float, Double, Character, String
+// swift는 데이터 타입에 엄격한 언어이다.
+
+//Bool(Boolean)
+var someBool:Bool = true
+
+//Int(int)
+var someInt:Int = -10
+
+//UInt(unsigned int 정수만 존재하는 int)
+var someUInt:UInt = 10
+
+//Float
+var someFloat:Float = 3.14
+someFloat = 3
+
+//Double
+var someDouble:Double = 3.14
+// someDouble = someFloat X
+
+//Character (유니코드로 표현하기 때문에 이모티콘도 가능)
+var someCharacter:Character = "A"
+
+//String (문자열)
+var someString:String = "Hello World!"
+var someString2:String = "Hello Swift!"
+
+print("\(someString) and \(someString2)")
+print(someString)
+print(someString2)
+
