7a2e05f655d3e4c6c930c113f575a0e6c15dcec9
jungwoo Lee <b5nnnal88@gmail.com>
2021-04-03 10:49:44 +0900

    swift-0403
    use navigation controller

---
+++ swift-practice/swift-practice/DetailController.swift
@@ -0,0 +1,16 @@
+//
+//  DetailController.swift
+//  swift-practice
+//
+//  Created by 이정우 on 2021/04/03.
+//
+
+import UIKit
+
+class DetailController : UIViewController{
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    
+}
--- swift-practice/swift-practice/ViewController.swift
+++ swift-practice/swift-practice/ViewController.swift
@@ -14,9 +14,22 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
     
-    //click event
-    @IBAction func Click_moveBtn(_ sender: Any) {
-        print("Click move")
+    @IBAction func Click_MoveBtn(_ sender: Any) {
+        //storyboard find controller : DetailController
+        //변수
+        //nil 값이 있을수도 있고 없을수도 있음
+        //옵셔널 바인딩
+        if let controller = self.storyboard?.instantiateViewController(identifier: "DetailController"){
+            //push controller = navigation
+            self.navigationController?.pushViewController(controller, animated: true)
+            
+        }
+        
+        
+        
+        
+        //move controller
+        
     }
     
 }
